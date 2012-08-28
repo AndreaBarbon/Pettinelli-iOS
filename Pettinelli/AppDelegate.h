f673a10db2a7c61903c248ef3ef60348751f181d
AndreaBarbon <andrea.3arbon@gmail.com>
2012-08-28 19:19:21 +0200

    Initial Commit

---
+++ Pettinelli/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pettinelli
+//
+//  Created by Andrea Barbon on 28/08/12.
+//  Copyright (c) 2012 fractalsoft srl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pettinelli/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pettinelli
+//
+//  Created by Andrea Barbon on 28/08/12.
+//  Copyright (c) 2012 fractalsoft srl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
